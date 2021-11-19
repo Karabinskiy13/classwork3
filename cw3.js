0462a983fa6b38ce2534841fcebbf19b1ff32d6a
Karabinskiy13 <ivan.karabinskiy1308@gmail.com>
2021-11-19 13:23:14 +0200

    Initial commit

---
+++ cw3.js
@@ -0,0 +1,45 @@
+// let numbs = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18]
+// i=0;
+// while (i<10){
+//     console.log(numbs[i]);
+//     i++
+// }
+// for ( let i=0; i<10; i++){
+//     console.log(numbs[i])
+// };
+// while  (i<10){
+//     if (i%2===1) {
+//         console.log(numbs[i])
+//     }
+//     i++
+// }
+//
+// for (let i=0; i<10; i++){
+//     if (i%2===1){
+//         console.log(numbs[i])
+//     }
+// }
+//
+// while  (i<10){
+//     if (i%2===0) {
+//         console.log(numbs[i])
+//     }
+//     i++
+// }
+//
+// for (let i=0; i<10; i++){
+//     if (i%2===0){
+//         console.log(numbs[i])
+//     }
+// }
+
+// for (let=i=0; i<10; i++){
+//     if (numbs[i]%3===0){
+//         numbs[i]='okten';
+//         console.log(numbs[i])
+//     }
+// }
+//
+// let numbs = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18]
+// numbs.reverse()
+// console.log(numbs)
